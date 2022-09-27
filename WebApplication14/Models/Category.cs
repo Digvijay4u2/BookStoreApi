d97a98f71feee4318b37dd77c9c3aa4d94650ac8
Digvijay4u2 <68144338+Digvijay4u2@users.noreply.github.com>
2022-09-27 14:23:38 +0530

    Add files via upload

---
+++ WebApplication14/Models/Category.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreApi.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int Position { get; set; }
+        public string CreatedAt { get; set; }
+
+        public Category()
+        {
+
+        }
+
+        public Category(int catId, string catName,string des,string Img,int pos,string createdAt)
+        {
+            this.CategoryId = catId;
+            this.CategoryName = catName;
+            this.Description = des;
+            this.Image = Img;
+            this.Position = pos;
+            this.CreatedAt = createdAt;
+        }
+    }
+
+
+}
---
+++ WebApplication14/Models/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStoreApi.Models
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAllBook();
+        Book GetBookById(int id);
+        Book AddBook(Book book);
+        string DeleteBook(int id);
+        string UpdateBook(int id,Book book);
+    }
+}
